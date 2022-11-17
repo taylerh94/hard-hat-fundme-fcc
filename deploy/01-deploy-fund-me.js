db0774960baf20d8702adf09a592dc8caaedb4a4
Tayler <tayler@Taylers-MBP.lan>
2022-11-16 18:51:45 -0800

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,47 @@
+// function deployFunc() {
+//     console.log("hello!")
+// }
+
+// module.exports.default = deployFunc
+
+// above is different way of accomplishing the below
+
+// module.exports = async (hre) =>  {
+//     const { getNamedAccounts, deployments } = hre
+// }
+
+// above is same as below
+
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    const args = [ethUsdPriceFeedAddress]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1
+    })
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+    log("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
+}
+
+module.exports.tags = ["all", "fundme"]
